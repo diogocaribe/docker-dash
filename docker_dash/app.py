a53de616e41e1fb53462a9f28a3d263fac0b2af2
Diogo Caribé <dogosousa@gmail.com>
2024-10-19 18:33:05 -0300

    refactor: setting ruff format and apply it in project

--- docker_dash/app.py
+++ docker_dash/app.py
@@ -1,6 +1,6 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+from dash import Dash, Input, Output, callback, dcc, html
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
@@ -19,7 +19,9 @@ app.layout = html.Div(
 )
 
 
-@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
+@callback(
+    Output("graph-content", "figure"), Input("dropdown-selection", "value")
+)
 def update_graph(value):
     dff = df[df.country == value]
     return px.line(dff, x="year", y="pop")
