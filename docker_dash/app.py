ae9657f4f56274ca7c15b3b46b1652e52d5327a7
Diogo Caribé <dogosousa@gmail.com>
2024-10-20 08:41:15 -0300

    refactor: add settings with pydantic-settings and .env

--- docker_dash/app.py
+++ docker_dash/app.py
@@ -2,9 +2,13 @@ import pandas as pd
 import plotly.express as px
 from dash import Dash, Input, Output, callback, dcc, html
 
+from docker_dash.settings import Settings
+
 df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
+    'https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv'
 )
+
+settings = Settings()
 
 app = Dash(__name__)
 
@@ -12,20 +16,20 @@ server = app.server
 
 app.layout = html.Div(
     [
-        html.H1(children="Title of Dash App", style={"textAlign": "center"}),
-        dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
-        dcc.Graph(id="graph-content"),
+        html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
+        dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+        dcc.Graph(id='graph-content'),
     ]
 )
 
 
 @callback(
-    Output("graph-content", "figure"), Input("dropdown-selection", "value")
+    Output('graph-content', 'figure'), Input('dropdown-selection', 'value')
 )
 def update_graph(value):
     dff = df[df.country == value]
-    return px.line(dff, x="year", y="pop")
+    return px.line(dff, x='year', y='pop')
 
 
-if __name__ == "__main__":
-    app.run_server(port=8050)
+if __name__ == '__main__':
+    app.run(port=8050, debug=settings.DASH_DEBUG_MODE)
---
+++ docker_dash/settings.py
@@ -0,0 +1,12 @@
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8', extra='ignore'
+    )
+
+    pg_dsn: PostgresDsn = 'postgres://user:pass@localhost:5432/foobar'
+
+    DASH_DEBUG_MODE: bool
